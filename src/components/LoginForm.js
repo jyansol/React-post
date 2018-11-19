c780ca7dab3bd43205dca19d7f6c62c1ba8d962f
jyansol <hahn0406@gmail.com>
2018-11-19 17:05:58 +0900

    Ref사용

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,10 +2,19 @@ import React from 'react';
 import api from '../api';
 
 export default class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    // DOM node를 가리키는 화살표다!
+    this.usernameRef = React.createRef();
+    this.passwordRef = React.createRef();
+  }
+
   async handleSubmit(e) {
     e.preventDefault();
-    const username = e.target.elements.username.value;
-    const password = e.target.elements.password.value;
+    const username = this.usernameRef.current.value;
+    const password = this.passwordRef.current.value;
+    // const password = e.target.elements.password.value;
+    // form을 안쓰면서 제어되는 구문을 쓰고 싶을때는 Ref를 반드시 써야함
     const res = await api.post('/users/login', {
       username,
       password,
@@ -19,8 +28,8 @@ export default class LoginForm extends React.Component {
       <React.Fragment>
         <form onSubmit={(e) => this.handleSubmit(e)}>
           <h1>Login</h1>
-          <input type="text" name="username" />
-          <input type="password" name="password" />
+          <input ref={this.usernameRef} type="text" name="username" />
+          <input ref={this.passwordRef} type="password" name="password" />
           <button>Login</button>
         </form>
         <button onClick={() => onRegister()}>sign up</button>
