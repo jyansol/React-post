cb615362ad2478a1ac4fcf93aaee4dd92c79db73
jyansol <hahn0406@gmail.com>
2018-11-22 16:26:43 +0900

    UserConsumer를 loginForm에 둘러쌈

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { UserConsumer } from '../contexts/UserContext';
 
-export default class LoginForm extends React.Component {
+class LoginForm extends React.Component {
   constructor(props) {
     super(props);
     // DOM node를 가리키는 화살표다!
@@ -23,29 +23,33 @@ export default class LoginForm extends React.Component {
   //   localStorage.setItem('token', res.data.token);
   // }
 
+  // Consumer로 받으려면, render()내부가 복잡해짐.
+  // props를 통해 onSubmit함수를 내려줌
+  handleSubmit(e) {
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
+    this.props.login(username, password);
+  }
   render() {
     const { onRegister } = this.props;
     return (
-      <UserConsumer>
-        {({ login }) => (
-          <React.Fragment>
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                const username = e.target.elements.username.value;
-                const password = e.target.elements.password.value;
-                login(username, password);
-              }}
-            >
-              <h1>Login</h1>
-              <input ref={this.usernameRef} type="text" name="username" />
-              <input ref={this.passwordRef} type="password" name="password" />
-              <button>Login</button>
-            </form>
-            <button onClick={() => onRegister()}>sign up</button>
-          </React.Fragment>
-        )}
-      </UserConsumer>
+      <React.Fragment>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <h1>Login</h1>
+          <input ref={this.usernameRef} type="text" name="username" />
+          <input ref={this.passwordRef} type="password" name="password" />
+          <button>Login</button>
+        </form>
+        <button onClick={() => onRegister()}>sign up</button>
+      </React.Fragment>
     );
   }
 }
+
+//함수형 컴포넌트 위에 class 앞에 export default지워줬음
+// render() 안에서 범위가 좁게 사용하던걸 export해줌으로써 props로 여러군데에서 사용
+// 받은 props를 loginForm에 내려주는 코드 => 사용법이 <loginForm/> 과 같다
+export default (props) => {
+  return <UserConsumer>{({ login }) => <LoginForm {...props} login={login} />}</UserConsumer>;
+};
